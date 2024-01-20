ee9f98595c2d3955b3cc4d2a1d67534686c8f6a3
Yida Zhang <yidazhang99@gmail.com>
2024-01-20 14:48:47 -0500

    Changed the response to a full HTML page instead of just a line of text.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
+		n, err := fmt.Fprintf(w, "<html><head><title>HelloWorld Example Page</title></head><body><h1>Hello, world!</h1>Welcome to the world of HTML.<p>This is one paragraph.</p><p>This is a second paragraph.</p></body></html>")
 		if err !=nil {
 			fmt.Println(err)
 		}
